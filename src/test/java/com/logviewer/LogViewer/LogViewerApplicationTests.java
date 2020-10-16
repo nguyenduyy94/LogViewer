c7c43943687e80056c91874ee9c592b8986704b3
nguyenduyy <duyy.uit@gmail.com>
2020-10-16 13:58:02 +0700

    init

---
+++ src/test/java/com/logviewer/LogViewer/LogViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logviewer.LogViewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogViewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
